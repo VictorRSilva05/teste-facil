2f5efff294098ceed092664ea6a9aab72f8749b4
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-02 20:25:05 -0300

    Adicionado projeto de IA

---
+++ TesteFacil.Infraestructure.AI.Gemini/DTOs/AlternativaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteFacil.Infraestrutura.IA.Gemini.DTOs;
+
+public class AlternativaDto
+{
+    public string Resposta { get; set; }
+    public bool Correta { get; set; }
+
+    public AlternativaDto() { }
+}
---
+++ TesteFacil.Infraestructure.AI.Gemini/DTOs/QuestaoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteFacil.Infraestrutura.IA.Gemini.DTOs;
+
+public class QuestaoDto
+{
+    public string Enunciado { get; set; }
+    public List<AlternativaDto> Alternativas { get; set; }
+
+    public QuestaoDto() { }
+}
