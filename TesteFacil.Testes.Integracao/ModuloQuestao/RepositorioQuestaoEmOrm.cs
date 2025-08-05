5c5b9ed54b036d67de348d11a865ddc07251ba8b
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-05 13:10:54 -0300

    Implementado testes de integração do módulo de Questões

--- TesteFacil.Testes.Integracao/ModuloQuestao/RepositorioQuestaoEmOrm.cs
+++ TesteFacil.Testes.Integracao/ModuloQuestao/RepositorioQuestaoEmOrm.cs
@@ -34,4 +34,96 @@ public sealed class RepositorioQuestaoEmOrm : TestFixture
 
         Assert.AreEqual(questao, registroSelecionado);
     }
+
+    [TestMethod]
+    public void Deve_Editar_Questao_Corretamente()
+    {
+        //Arrange
+        var disciplina = Builder<Disciplina>.CreateNew()
+            .With(d => d.Nome = "Matemática")
+            .Persist();
+
+        var materia = Builder<Materia>.CreateNew()
+            .With(m => m.Nome = "Quatro Operações")
+            .With(m => m.Disciplina = disciplina)
+            .Persist();
+
+        var questao = new Questao("Quanto é 2 + 2?", materia);
+
+        repositorioQuestao?.Cadastrar(questao);
+        dbContext?.SaveChanges();
+
+        var questaoEditada = new Questao("Quanto é 5 + 9?", materia);
+
+        //Act
+        var conseguiuEditar = repositorioQuestao?.Editar(questao.Id, questaoEditada);
+        dbContext?.SaveChanges();
+
+        //Assert
+        var registroSelecionado = repositorioQuestao?.SelecionarRegistroPorId(questao.Id);
+
+        Assert.IsTrue(conseguiuEditar);
+        Assert.AreEqual(questao, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Questao_Corretamente()
+    {
+        //Arrange
+        var disciplina = Builder<Disciplina>.CreateNew()
+            .With(d => d.Nome = "Matemática")
+            .Persist();
+
+        var materia = Builder<Materia>.CreateNew()
+            .With(m => m.Nome = "Quatro Operações")
+            .With(m => m.Disciplina = disciplina)
+            .Persist();
+
+        var questao = new Questao("Quanto é 2 + 2?", materia);
+
+        repositorioQuestao?.Cadastrar(questao);
+        dbContext?.SaveChanges();
+
+        //Act
+        var conseguiuExcluir = repositorioQuestao?.Excluir(questao.Id);
+        dbContext?.SaveChanges();
+
+        //Assert
+        var registroSelecionado = repositorioQuestao?.SelecionarRegistroPorId(questao.Id);
+
+        Assert.IsTrue(conseguiuExcluir);
+        Assert.IsNull(registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Questoes_Corretamente()
+    {
+        //Arrange
+        var disciplina = Builder<Disciplina>.CreateNew()
+            .With(d => d.Nome = "Matemática")
+            .Persist();
+
+        var materias = Builder<Materia>.CreateListOfSize(3)
+            .All()
+            .With(m => m.Disciplina = disciplina)
+            .Persist();
+
+        var questao = new Questao("Quanto é 2 + 2?", materias[0]);
+        var questao2 = new Questao("Quanto é 85 - 42?", materias[1]);
+        var questao3 = new Questao("Quanto é 15 / 5?", materias[2]);
+
+        List<Questao> registrosEsperados = [questao, questao2, questao3];
+
+        repositorioQuestao?.CadastrarEntidades(registrosEsperados);
+        dbContext?.SaveChanges();
+
+        //Act
+        var registroRecebidos = repositorioQuestao?.SelecionarRegistros();
+
+        //Assert
+        var registroEsperadosOrdenados = registrosEsperados
+            .OrderBy(x => x.Enunciado).ToList();
+
+        CollectionAssert.AreEqual(registroEsperadosOrdenados, registroRecebidos);
+    }
 }
