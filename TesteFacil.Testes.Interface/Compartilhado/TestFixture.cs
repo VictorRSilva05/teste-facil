fbb350c3762e72ccdc248495b454e18c3acfc5ce
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-13 14:05:54 -0300

    Implementado projeto de testes de interface

---
+++ TesteFacil.Testes.Interface/Compartilhado/TestFixture.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TesteFacil.Testes.Interface.Compartilhado;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected static IWebDriver? driver;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        InicializarWebDriver();
+    }
+
+    [TestCleanup]
+    public void EncerrarTestes()
+    {
+        EncerrarWebDriver();
+    }
+
+    private static void InicializarWebDriver()
+    {
+        driver = new FirefoxDriver();
+    }
+
+    private static void EncerrarWebDriver()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+}
---
+++ TesteFacil.Testes.Interface/ModuloDisciplina/DisciplinaInterfaceTests.cs
@@ -0,0 +1,14 @@
+﻿using TesteFacil.Testes.Interface.Compartilhado;
+
+namespace TesteFacil.Testes.Interface.ModuloDisciplina;
+
+[TestClass]
+[TestCategory("Tests de interface de Disciplina")]
+public sealed class DisciplinaInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Test()
+    {
+
+    }
+}
