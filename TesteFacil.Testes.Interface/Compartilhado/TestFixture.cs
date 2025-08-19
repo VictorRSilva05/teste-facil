7cd0efe871604b5cf6f96fec689f0f6b495fe1cb
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-19 09:13:57 -0300

    Adicionado configuração para contâiner de teste no TestFixture

--- TesteFacil.Testes.Interface/Compartilhado/TestFixture.cs
+++ TesteFacil.Testes.Interface/Compartilhado/TestFixture.cs
@@ -1,5 +1,10 @@
-﻿using OpenQA.Selenium;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.EntityFrameworkCore.Storage;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
 using TesteFacil.Infraestrutura.Orm.Compartilhado;
 using static System.Net.WebRequestMethods;
 
@@ -14,15 +19,22 @@ public abstract class TestFixture
     protected static string enderecoBase = "https://localhost:7056";
     private static string connectionString = "Host=localhost;Port=5432;Database=AcademiaDoProgramadorDb;Username=postgres;Password=YourStrongPassword";
 
+    private static IDatabaseContainer? dbContainer;
+    private readonly static int dbPort = 5432;
+
     [AssemblyInitialize]
-    public static void ConfigurarTestes(TestContext _)
+    public static async Task ConfigurarTestes(TestContext _)
     {
+        await InicializarBancoDadosAsync();
+
         InicializarWebDriver();
     }
 
     [AssemblyCleanup]
-    public static void EncerrarTestes()
+    public static async Task EncerrarTestes()
     {
+        await EncerrarBancoDadosAsync();
+
         EncerrarWebDriver();
     }
 
@@ -34,6 +46,21 @@ public abstract class TestFixture
         ConfigurarTabelas(dbContext);
     }
 
+    private static async Task InicializarBancoDadosAsync()
+    {
+        dbContainer = new PostgreSqlBuilder()
+       .WithImage("postgres:16")
+       .WithPortBinding(dbPort, true)
+       .WithName("teste-facil-testedb")
+       .WithDatabase("TesteFacilTesteDb")
+       .WithUsername("postgres")
+       .WithPassword("YourStrongPassword")
+       .WithCleanUp(true)
+       .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(dbPort))
+       .Build();
+
+        await dbContainer.StartAsync();
+    }
 
     private static void InicializarWebDriver()
     {
@@ -42,7 +69,12 @@ public abstract class TestFixture
 
         driver = new FirefoxDriver(options);
     }
-
+    
+    private static async Task EncerrarBancoDadosAsync()
+    {
+        if(dbContainer is not null)
+           await dbContainer.DisposeAsync();
+    }
     private static void EncerrarWebDriver()
     {
         driver?.Quit();
